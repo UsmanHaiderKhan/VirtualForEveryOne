a264df0dffd4616ddaf3aa9888a6a5adb0c307f1
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-10-05 14:25:33 +0500

    Update added Propertise for admin

--- VirtualForEveryOne/Models/Admin.cs
+++ VirtualForEveryOne/Models/Admin.cs
@@ -1,11 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace VirtualForEveryOne.Models
+﻿namespace VirtualForEveryOne.Models
 {
     public class Admin
     {
+        public int Id { get; set; }
+        public string name { get; set; }
+        public string password { get; set; }
+
+
     }
 }
