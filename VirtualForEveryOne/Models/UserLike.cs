3b2a72f74eecce32e62c1a461deedf8a017eeabf
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-10-06 10:41:10 +0500

    Update added User Likes or Dislikes Page

--- VirtualForEveryOne/Models/UserLike.cs
+++ VirtualForEveryOne/Models/UserLike.cs
@@ -1,11 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace VirtualForEveryOne.Models
+﻿namespace VirtualForEveryOne.Models
 {
     public class UserLike
     {
+        public int Id { get; set; }
+        public string username { get; set; }
+        public string question { get; set; }
+        public string image { get; set; }
+
+        public string answer { get; set; }
+        public int like { get; set; }
+        public int dislike { get; set; }
+
     }
 }
