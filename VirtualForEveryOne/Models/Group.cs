08acfab165f23525d5999c61e97789ebab2a89f6
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-10-06 10:37:28 +0500

    UPdate the group property

--- VirtualForEveryOne/Models/Group.cs
+++ VirtualForEveryOne/Models/Group.cs
@@ -1,11 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace VirtualForEveryOne.Models
+﻿namespace VirtualForEveryOne.Models
 {
     public class Group
     {
+        public int Id { get; set; }
+        public int groupid { get; set; }
+        public string groupname { get; set; }
+        public string groupadmin { get; set; }
+        public string image { get; set; }
+        public string info { get; set; }
+
     }
 }
