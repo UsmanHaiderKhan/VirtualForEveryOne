060f9adb26e21b5bd030b8fab902b278538da827
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-10-06 10:36:33 +0500

    UPdate Added Property of the faq class which is required

--- VirtualForEveryOne/Models/Faq.cs
+++ VirtualForEveryOne/Models/Faq.cs
@@ -1,11 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace VirtualForEveryOne.Models
+﻿namespace VirtualForEveryOne.Models
 {
     public class Faq
     {
+        public int Id { get; set; }
+        public string faq1 { get; set; }
+        public string faqAnswer { get; set; }
+
     }
 }
