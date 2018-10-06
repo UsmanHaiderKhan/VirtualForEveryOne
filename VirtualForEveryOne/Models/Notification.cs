88be397169df2016309b03afeb38127cba35fe6e
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-10-06 10:38:11 +0500

    Update Added Notifcation propertise

--- VirtualForEveryOne/Models/Notification.cs
+++ VirtualForEveryOne/Models/Notification.cs
@@ -1,11 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 
 namespace VirtualForEveryOne.Models
 {
     public class Notification
     {
+        public int Id { get; set; }
+        public string postid { get; set; }
+        public string username { get; set; }
+        public string notifier { get; set; }
+        public string type { get; set; }
+        public bool state { get; set; }
+        public DateTime time { get; set; }
+
+
     }
 }
