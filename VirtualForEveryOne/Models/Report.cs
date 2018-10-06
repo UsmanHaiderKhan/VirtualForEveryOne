207068dab1dbce422dc38268b1258b9801a8f96e
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-10-06 10:39:22 +0500

    Update the report of that class which is required Nescessary

--- VirtualForEveryOne/Models/Report.cs
+++ VirtualForEveryOne/Models/Report.cs
@@ -1,11 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 
 namespace VirtualForEveryOne.Models
 {
     public class Report
     {
+        public int Id { get; set; }
+        public string username { get; set; }
+        public string postid { get; set; }
+        public string comments { get; set; }
+        public string status { get; set; }
+        public DateTime time { get; set; }
+
     }
 }
