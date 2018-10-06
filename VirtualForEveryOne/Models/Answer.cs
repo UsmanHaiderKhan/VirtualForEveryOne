6715cc5e08536f51519807d5255dc00f6fb0c66a
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-10-06 10:36:10 +0500

    Update the Answer Class

--- VirtualForEveryOne/Models/Answer.cs
+++ VirtualForEveryOne/Models/Answer.cs
@@ -1,11 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 
 namespace VirtualForEveryOne.Models
 {
     public class Answer
     {
+        public int Id { get; set; }
+        public string username { get; set; }
+        public string a_username { get; set; }
+        public string question { get; set; }
+        public string image { get; set; }
+        public string ans { get; set; }
+        public string s_profilepic { get; set; }
+        public string a_profilepic { get; set; }
+        public int like { get; set; }
+        public int dislike { get; set; }
+        public DateTime time { get; set; }
+        public string status { get; set; }
+
     }
 }
