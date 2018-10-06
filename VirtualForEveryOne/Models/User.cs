178e68cc777db7a73fe8c70f4ecd35f47df43710
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-10-06 10:40:45 +0500

    Update added Property to get the user mendatory information

--- VirtualForEveryOne/Models/User.cs
+++ VirtualForEveryOne/Models/User.cs
@@ -1,11 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace VirtualForEveryOne.Models
+﻿namespace VirtualForEveryOne.Models
 {
     public class User
     {
+        public int Id { get; set; }
+        public string username { get; set; }
+        public string fullname { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public string dob { get; set; }
+        public string skills { get; set; }
+        public string gender { get; set; }
+        public string bio { get; set; }
+        public string profilepic { get; set; }
+        public string coverpic { get; set; }
+        public bool status { get; set; }
+
     }
 }
