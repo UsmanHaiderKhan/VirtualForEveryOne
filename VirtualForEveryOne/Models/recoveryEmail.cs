84f7d2d9e8a589208003ec9308d5c0411937cb20
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2019-02-04 18:48:23 -1200

    Update added recoveryEmail Model Class

---
+++ VirtualForEveryOne/Models/recoveryEmail.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VirtualForEveryOne.Models
+{
+    public class recoveryEmail
+    {
+        [Required, Display(Name = "Email"), EmailAddress]
+        public string Email { get; set; }
+    }
+}
