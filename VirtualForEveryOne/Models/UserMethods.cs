9ee527da3f1b77d6ce02c8b2a66ec9e0f0c260e9
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2019-06-15 03:12:29 -0700

    Update added Group creating Module

---
+++ VirtualForEveryOne/Models/UserMethods.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+
+namespace VirtualForEveryOne.Models
+{
+    public class UserMethods
+    {
+
+        private VirtualContext db = new VirtualContext();
+
+        public User GetUser()
+        {
+            using (db)
+            {
+                return (from c in db.Users select c).FirstOrDefault();
+            }
+        }
+    }
+}
