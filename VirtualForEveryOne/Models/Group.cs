621623936d6eca19213fba5ea3156c3cfa60cf10
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2019-06-16 01:15:46 -0700

    Update Applied Model Vslidation On GRoup Class

--- VirtualForEveryOne/Models/Group.cs
+++ VirtualForEveryOne/Models/Group.cs
@@ -1,12 +1,23 @@
-﻿namespace VirtualForEveryOne.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VirtualForEveryOne.Models
 {
     public class Group
     {
         public int Id { get; set; }
+        [Required]
+
         public int groupid { get; set; }
+        [Required]
+        [MaxLength(20), MinLength(3)]
         public string groupname { get; set; }
+        [Required]
+        [MaxLength(20), MinLength(3)]
+
         public string groupadmin { get; set; }
+
         public string image { get; set; }
+        [Required]
         public string info { get; set; }
 
     }
