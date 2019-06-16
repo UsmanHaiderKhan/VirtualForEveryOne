561104161dd02f4991dd4d72c7998a3f90fcfaac
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2019-06-16 01:14:44 -0700

    Update applied User Model Validation

--- VirtualForEveryOne/Models/User.cs
+++ VirtualForEveryOne/Models/User.cs
@@ -1,13 +1,26 @@
-﻿namespace VirtualForEveryOne.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VirtualForEveryOne.Models
 {
     public class User
     {
         public int Id { get; set; }
+        [Required]
+
+        [StringLength(20, MinimumLength = 3)]
+
         public string username { get; set; }
+        [Required]
+        [StringLength(20, MinimumLength = 3)]
         public string fullname { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [RegularExpression(@"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$", ErrorMessage = "Email is not valid.")]
         public string email { get; set; }
+
         public string password { get; set; }
         public string dob { get; set; }
+        [Required]
         public string skills { get; set; }
         public string gender { get; set; }
         public string bio { get; set; }
