44d92d82c583f1a47299aa0669fdc745822504a6
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2019-07-04 22:44:18 +0500

    Update added New UserMethods

--- VirtualForEveryOne/Models/UserMethods.cs
+++ VirtualForEveryOne/Models/UserMethods.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 
 namespace VirtualForEveryOne.Models
@@ -29,6 +30,31 @@ namespace VirtualForEveryOne.Models
             using (db)
             {
                 return (from c in db.Groups where c.Id == id select c).FirstOrDefault();
+            }
+        }
+
+        public List<GroupMember> GetGroupMembers(int groupid)
+        {
+            using (db)
+            {
+                return (from v in db.GroupMembers where v.groudid == groupid select v).ToList();
+            }
+        }
+
+        public Answer GetAnswersById(int id)
+        {
+            using (db)
+            {
+                return (from v in db.Answers where v.Id == id select v).SingleOrDefault();
+            }
+        }
+
+        public void UpdateAnswer(Answer answer)
+        {
+            using (db)
+            {
+                db.Entry(answer).State = EntityState.Modified;
+                db.SaveChanges();
             }
         }
     }
